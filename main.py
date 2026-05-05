0f1781c6ceb803bf1e87606c979f9c8617cd01ad
HaRsh-init <hrj9988@gmail.com>
2026-05-05 16:26:31 +0530

    updated schema and main as database integrated

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-from fastapi import FastAPI, Request, HTTPException, status
+from fastapi import FastAPI, Request, HTTPException, status, Depends
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+import sqlalchemy
 from starlette.exceptions import HTTPException as StarletteHTTPException
+
+from schemas import postCreate, postResponse
+
+from typing import Annotated
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+import models
+from database import get_db, engine, Base
+
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -40,11 +51,24 @@ def post_page(request: Request,post_id: int):
 
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= "Page Not Found..")
 
-@app.get("/api/posts")
+@app.get("/api/posts", response_model= list[postResponse])
 def get_posts():
     return posts
 
-@app.get("/api/posts/{post_id}")
+@app.post("/api/posts", response_model= postCreate, status_code=status.HTTP_201_CREATED)
+def create_post(post: postCreate):
+    new_id = max(post.get("id", 0) for post in posts) + 1 if posts else 1
+    new_post = {
+        "id": new_id,
+        "author": post.author,
+        "title": post.title,
+        "content": post.content,
+        "date_posted": "April 22, 2025",
+    }
+    posts.append(new_post)
+    return new_post
+
+@app.get("/api/posts/{post_id}", response_model= postResponse)
 def get_post(post_id: int):
     for post in posts:
         if post.get("id") == post_id:
