f4d9de64d701b37650a21097ce1a2b93b10a2ab6
HaRsh-init <hrj9988@gmail.com>
2026-06-03 15:12:10 +0530

    added authentication and registration and login with jwt

--- schemas.py
+++ schemas.py
@@ -7,20 +7,26 @@ class UserBase(BaseModel):
     email: EmailStr = Field(max_length=100)
 
 class UserCreate(UserBase):
-    #password: str = Field(min_length=6, max_length=100)
-    pass
+    password: str = Field(min_length=8, max_length=100)
 
-class UserResponse(UserBase):
+class UserPublic(BaseModel):
     model_config = ConfigDict(from_attributes=True)
     id: int
+    username: str
     image_file: str | None
     image_path: str
+
+class UserPrivate(UserPublic):
+    email: EmailStr
 
 class UserUpdate(BaseModel):
     username: str | None = Field(default=None, min_length=2, max_length=50)
     email: EmailStr | None = Field(default=None, max_length=100)
     image_file: str | None = Field(default=None)
 
+class Token(BaseModel):
+    access_token: str
+    token_type: str
 
 class PostBase(BaseModel):
     #model_config = ConfigDict(from_attributes=True)
@@ -41,4 +47,4 @@ class PostResponse(PostBase):
     id: int = Field()
     user_id: int = Field()
     date_posted: datetime = Field()
-    author: UserResponse
+    author: UserPublic
