1f83f613869474527c9acebb3fd953fc86b02671
HaRsh-init <hrj9988@gmail.com>
2026-06-10 14:58:50 +0530

    account authentication added based on user

--- main.py
+++ main.py
@@ -122,7 +122,7 @@ async def register_page(request: Request):
         {"title": "Register"},
     )
 
-@app.get("/register", include_in_schema=False)
+@app.get("/account", include_in_schema=False)
 async def account_page(request: Request):
     return templates.TemplateResponse(
         request,
